294cbe173fca9141911b8caef95e984be0fae4f9
shubhankar2203 <64438573+shubhankar2203@users.noreply.github.com>
2024-02-16 21:42:39 +0800

    Add files via upload

---
+++ ifElif.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 != 0:
+        print("Weird")
+        
+    elif n % 2 == 0 & n >= 2 & n <= 5:
+        print("Not Weird")
+    
+    elif n % 2 == 0 & n >= 6 & n <= 20:
+        print("Weird")
+        
+    elif n % 2 == 0 & n > 20:
+        print("Not Weird")    
